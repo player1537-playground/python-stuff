9fbe9588f0ef8c4ab52d896e2d097c559713a004
Tanner Hobson <thobson125@gmail.com>
2013-05-24 17:10:35 -0400

    added display order of operations to proxy.py

--- vector/proxy.py
+++ vector/proxy.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 
 class Oper(object):
-    def __init__(self, symbol, fun):
+    def __init__(self, symbol, oop, fun):
         self.symbol = symbol
         self.fun = fun
+        self.oop = oop
     def __call__(self, left, right):
         return self.fun(left, right)
     def __repr__(self):
         return self.symbol
     def __str__(self):
         return self.__repr__()
+    def __cmp__(self, other):
+        if self.oop < other.oop:
+            return -1
+        elif self.oop > other.oop:
+            return 1
+        else:
+            return 0
 
 class OperMaster(object):
     def __init__(self):
@@ -21,14 +29,15 @@ class OperMaster(object):
         return self.opers[symbol]
 
 opers = OperMaster()
+# [**] > [*,/,%,//] > [-,+] 
 opers + \
-    Oper("+", lambda a,b: a + b) + \
-    Oper("-", lambda a,b: a - b) + \
-    Oper("*", lambda a,b: a * b) + \
-    Oper("/", lambda a,b: a / b) + \
-    Oper("^", lambda a,b: a ** b) + \
-    Oper("%", lambda a,b: a % b) + \
-    Oper("//", lambda a,b: a // b)
+    Oper("+", 0, lambda a,b: a + b) + \
+    Oper("-", 0, lambda a,b: a - b) + \
+    Oper("*", 1, lambda a,b: a * b) + \
+    Oper("/", 1, lambda a,b: a / b) + \
+    Oper("^", 2, lambda a,b: a ** b) + \
+    Oper("%", 1, lambda a,b: a % b) + \
+    Oper("//", 1, lambda a,b: a // b)
 
 
 class ProxyNode(object):
@@ -60,12 +69,14 @@ class ProxyNode(object):
             return self
         return ProxyNode("-", self, other)
     def __rsub__(self, scalar):
-        if other == 0:
-            return self
+        if scalar == 0:
+            return -self
         return ProxyNode("-", scalar, self)
     def __mul__(self, other):
         if other == 1:
             return self
+        if other == 0:
+            return 0
         return ProxyNode("*", self, other)
     def __neg__(self):
         return self * -1
@@ -80,22 +91,38 @@ class ProxyNode(object):
     def __str__(self):
         return self.__repr__()
     def __repr__(self):
+        # 3+2*x**1+7
+        # 2*(3-x)
         values = (str(self.left), self.oper, str(self.right))
         #print [type(self.left), type(self), type(self.right)]
         if isinstance(self.left, Proxy):
             if isinstance(self.right, Proxy):
+                # x . y
                 return "%s%s%s" % values
             else:
                 if isinstance(self.right, ProxyNode):
-                    if self.right.oper == self.oper:
+                    # x . (3 + y) or x . 2*y
+                    if opers[self.right.oper] >= opers[self.oper]:
                         return "%s%s%s" % values
                     else:
                         return "%s%s(%s)" % values
                 else:
+                    # x . 1
                     return "%s%s%s" % values
         else:
-            if isinstance(self.left, ProxyNode) and self.left.oper == self.oper:
+            if isinstance(self.right, Proxy):
+                # 2 . y
                 return "%s%s%s" % values
+            else:
+                if isinstance(self.left, ProxyNode):
+                    # (3 + x) . y or 2*x . y
+                    if opers[self.left.oper] >= opers[self.oper]:
+                        return "%s%s%s" % values
+                    else:
+                        return "(%s)%s%s" % values
+                else:
+                    # 2 . 3
+                    return "%s%s%s" % values
         return "(%s)%s(%s)" % values
     def __rpow__(self, base):
         if other == 1:
@@ -139,3 +166,7 @@ print x+x
 print x+x+x
 print x+3+2
 print 2+x+3
+print x+3
+print ((1)-((((0)-((((1)-((x)*(7)))+(((0)-((x)*(0)))/(1)))*(2)))+(((1)-((((1)-((x)*(7)))+(((0)-((x)*(0)))/(1)))*(1)))/(7)))*(1)))+(((1)-((((0)-((((1)-((x)*(7)))+(((0)-((x)*(0)))/(1)))*(2)))+(((1)-((((1)-((x)*(7)))+(((0)-((x)*(0)))/(1)))*(1)))/(7)))*(7)))/(15))
+print "((1)-((((0)-((((1)-((x)*(7)))+(((0)-((x)*(0)))/(1)))*(2)))+(((1)-((((1)-((x)*(7)))+(((0)-((x)*(0)))/(1)))*(1)))/(7)))*(1)))+(((1)-((((0)-((((1)-((x)*(7)))+(((0)-((x)*(0)))/(1)))*(2)))+(((1)-((((1)-((x)*(7)))+(((0)-((x)*(0)))/(1)))*(1)))/(7)))*(7)))/(15))"
+print opers['+'], opers['*'], opers['+'] < opers['*']
